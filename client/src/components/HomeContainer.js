cade64b4fb86b7a2d8e26c5f9c6eeae39b496da1
iTh1nk <fnchao@hotmail.com>
2020-05-05 19:20:03 -0700

    setup home page

--- client/src/components/HomeContainer.js
+++ client/src/components/HomeContainer.js
@@ -1,17 +1,3 @@
-// import React from "react";
-
-// import HomeLeft from "./HomeLeft";
-// import HomeRight from "./HomeRight";
-
-// export default function HomeContainer() {
-//   return (
-//     <>
-//       <HomeLeft />
-//       <HomeRight />
-//     </>
-//   );
-// }
-
 import _ from "lodash";
 import React, { Component, createRef } from "react";
 import {
@@ -31,132 +17,132 @@ export default class StickyExampleOffset extends Component {
 
   render() {
     return (
-      <Grid centered columns={2}>
-        <Grid.Column>
+      <>
+        <Grid centered>
           <Ref innerRef={this.contextRef}>
-            <Segment>
-              In a professional context it often happens that private or
-              corporate clients corder a publication to be made and presented
-              with the actual content still not being ready. Think of a news
-              blog that's filled with content hourly on the day of going live.
-              However, reviewers tend to be distracted by comprehensible
-              content, say, a random text copied from a newspaper or the
-              internet. The are likely to focus on the text, disregarding the
-              layout and its elements. Besides, random text risks to be
-              unintendedly humorous or offensive, an unacceptable risk in
-              corporate environments. Lorem ipsum and its many variants have
-              been employed since the early 1960ies, and quite likely since the
-              sixteenth century.
-              In a professional context it often happens that private or
-              corporate clients corder a publication to be made and presented
-              with the actual content still not being ready. Think of a news
-              blog that's filled with content hourly on the day of going live.
-              However, reviewers tend to be distracted by comprehensible
-              content, say, a random text copied from a newspaper or the
-              internet. The are likely to focus on the text, disregarding the
-              layout and its elements. Besides, random text risks to be
-              unintendedly humorous or offensive, an unacceptable risk in
-              corporate environments. Lorem ipsum and its many variants have
-              been employed since the early 1960ies, and quite likely since the
-              sixteenth century.
-              In a professional context it often happens that private or
-              corporate clients corder a publication to be made and presented
-              with the actual content still not being ready. Think of a news
-              blog that's filled with content hourly on the day of going live.
-              However, reviewers tend to be distracted by comprehensible
-              content, say, a random text copied from a newspaper or the
-              internet. The are likely to focus on the text, disregarding the
-              layout and its elements. Besides, random text risks to be
-              unintendedly humorous or offensive, an unacceptable risk in
-              corporate environments. Lorem ipsum and its many variants have
-              been employed since the early 1960ies, and quite likely since the
-              sixteenth century.
-              In a professional context it often happens that private or
-              corporate clients corder a publication to be made and presented
-              with the actual content still not being ready. Think of a news
-              blog that's filled with content hourly on the day of going live.
-              However, reviewers tend to be distracted by comprehensible
-              content, say, a random text copied from a newspaper or the
-              internet. The are likely to focus on the text, disregarding the
-              layout and its elements. Besides, random text risks to be
-              unintendedly humorous or offensive, an unacceptable risk in
-              corporate environments. Lorem ipsum and its many variants have
-              been employed since the early 1960ies, and quite likely since the
-              sixteenth century.
-              In a professional context it often happens that private or
-              corporate clients corder a publication to be made and presented
-              with the actual content still not being ready. Think of a news
-              blog that's filled with content hourly on the day of going live.
-              However, reviewers tend to be distracted by comprehensible
-              content, say, a random text copied from a newspaper or the
-              internet. The are likely to focus on the text, disregarding the
-              layout and its elements. Besides, random text risks to be
-              unintendedly humorous or offensive, an unacceptable risk in
-              corporate environments. Lorem ipsum and its many variants have
-              been employed since the early 1960ies, and quite likely since the
-              sixteenth century.
-              In a professional context it often happens that private or
-              corporate clients corder a publication to be made and presented
-              with the actual content still not being ready. Think of a news
-              blog that's filled with content hourly on the day of going live.
-              However, reviewers tend to be distracted by comprehensible
-              content, say, a random text copied from a newspaper or the
-              internet. The are likely to focus on the text, disregarding the
-              layout and its elements. Besides, random text risks to be
-              unintendedly humorous or offensive, an unacceptable risk in
-              corporate environments. Lorem ipsum and its many variants have
-              been employed since the early 1960ies, and quite likely since the
-              sixteenth century.
-              In a professional context it often happens that private or
-              corporate clients corder a publication to be made and presented
-              with the actual content still not being ready. Think of a news
-              blog that's filled with content hourly on the day of going live.
-              However, reviewers tend to be distracted by comprehensible
-              content, say, a random text copied from a newspaper or the
-              internet. The are likely to focus on the text, disregarding the
-              layout and its elements. Besides, random text risks to be
-              unintendedly humorous or offensive, an unacceptable risk in
-              corporate environments. Lorem ipsum and its many variants have
-              been employed since the early 1960ies, and quite likely since the
-              sixteenth century.
-              In a professional context it often happens that private or
-              corporate clients corder a publication to be made and presented
-              with the actual content still not being ready. Think of a news
-              blog that's filled with content hourly on the day of going live.
-              However, reviewers tend to be distracted by comprehensible
-              content, say, a random text copied from a newspaper or the
-              internet. The are likely to focus on the text, disregarding the
-              layout and its elements. Besides, random text risks to be
-              unintendedly humorous or offensive, an unacceptable risk in
-              corporate environments. Lorem ipsum and its many variants have
-              been employed since the early 1960ies, and quite likely since the
-              sixteenth century.
-              In a professional context it often happens that private or
-              corporate clients corder a publication to be made and presented
-              with the actual content still not being ready. Think of a news
-              blog that's filled with content hourly on the day of going live.
-              However, reviewers tend to be distracted by comprehensible
-              content, say, a random text copied from a newspaper or the
-              internet. The are likely to focus on the text, disregarding the
-              layout and its elements. Besides, random text risks to be
-              unintendedly humorous or offensive, an unacceptable risk in
-              corporate environments. Lorem ipsum and its many variants have
-              been employed since the early 1960ies, and quite likely since the
-              sixteenth century.
-              <Rail position="left">
-                {_.times(3, (i) => (
-                  <Placeholder key={i} />
-                ))}
+            <Grid.Row style={{marginLeft: "15em"}}>
+              {/* <Grid.Column width={2}>
+                <Rail attached>
+                  <Sticky context={this.contextRef} offset={100}>
+                    In a professional context it often happens that private or
+                    corporate clients corder a publication to be made and
+                    presented with the actual content still not being ready.
+                    Think of a news blog that's filled with content hourly on
+                    the day of going live. However, reviewers tend to be
+                    distracted by comprehensible content, say, a random text
+                    copied from a newspaper or the internet. The are likely to
+                    focus on the text, disregarding the layout and its elements.
+                    Besides, random text risks to be unintendedly humorous or
+                    offensive, an unacceptable risk in corporate environments.
+                    Lorem ipsum and its many variants have been employed since
+                    the early 1960ies, and quite likely since the
+                  </Sticky>
+                </Rail>
+              </Grid.Column> */}
+              <Grid.Column width={8}>
+                <Segment>
+                  In a professional context it often happens that private or
+                  corporate clients corder a publication to be made and
+                  presented with the actual content still not being ready. Think
+                  of a news blog that's filled with content hourly on the day of
+                  going live. However, reviewers tend to be distracted by
+                  comprehensible content, say, a random text copied from a
+                  newspaper or the internet. The are likely to focus on the
+                  text, disregarding the layout and its elements. Besides,
+                  random text risks to be unintendedly humorous or offensive, an
+                  unacceptable risk in corporate environments. Lorem ipsum and
+                  its many variants have been employed since the early 1960ies,
+                  and quite likely since the sixteenth century. In a
+                  professional context it often happens that private or
+                  corporate clients corder a publication to be made and
+                  presented with the actual content still not being ready. Think
+                  of a news blog that's filled with content hourly on the day of
+                  going live. However, reviewers tend to be distracted by
+                  comprehensible content, say, a random text copied from a
+                  newspaper or the internet. The are likely to focus on the
+                  text, disregarding the layout and its elements. Besides,
+                  random text risks to be unintendedly humorous or offensive, an
+                  unacceptable risk in corporate environments. Lorem ipsum and
+                  its many variants have been employed since the early 1960ies,
+                  and quite likely since the sixteenth century. In a
+                  professional context it often happens that private or
+                  corporate clients corder a publication to be made and
+                  presented with the actual content still not being ready. Think
+                  of a news blog that's filled with content hourly on the day of
+                  going live. However, reviewers tend to be distracted by
+                  comprehensible content, say, a random text copied from a
+                  newspaper or the internet. The are likely to focus on the
+                  text, disregarding the layout and its elements. Besides,
+                  random text risks to be unintendedly humorous or offensive, an
+                  unacceptable risk in corporate environments. Lorem ipsum and
+                  its many variants have been employed since the early 1960ies,
+                  and quite likely since the sixteenth century. In a
+                  professional context it often happens that private or
+                  corporate clients corder a publication to be made and
+                  presented with the actual content still not being ready. Think
+                  of a news blog that's filled with content hourly on the day of
+                  going live. However, reviewers tend to be distracted by
+                  comprehensible content, say, a random text copied from a
+                  newspaper or the internet. The are likely to focus on the
+                  text, disregarding the layout and its elements. Besides,
+                  random text risks to be unintendedly humorous or offensive, an
+                  unacceptable risk in corporate environments. Lorem ipsum and
+                  its many variants have been employed since the early 1960ies,
+                  and quite likely since the sixteenth century. In a
+                  professional context it often happens that private or
+                  corporate clients corder a publication to be made and
+                  presented with the actual content still not being ready. Think
+                  of a news blog that's filled with content hourly on the day of
+                  going live. However, reviewers tend to be distracted by
+                  comprehensible content, say, a random text copied from a
+                  newspaper or the internet. The are likely to focus on the
+                  text, disregarding the layout and its elements. Besides,
+                  random text risks to be unintendedly humorous or offensive, an
+                  unacceptable risk in corporate environments. Lorem ipsum and
+                  its many variants have been employed since the early 1960ies,
+                  and quite likely since the sixteenth century. In a
+                  professional context it often happens that private or
+                  corporate clients corder a publication to be made and
+                  presented with the actual content still not being ready. Think
+                  of a news blog that's filled with content hourly on the day of
+                  going live. However, reviewers tend to be distracted by
+                  comprehensible content, say, a random text copied from a
+                  newspaper or the internet. The are likely to focus on the
+                  text, disregarding the layout and its elements. Besides,
+                  random text risks to be unintendedly humorous or offensive, an
+                  unacceptable risk in corporate environments. Lorem ipsum and
+                  its many variants have been employed since the early 1960ies,
+                  and quite likely since the sixteenth century. In a
+                  professional context it often happens that private or
+                  corporate clients corder a publication to be made and
+                  presented with the actual content still not being ready. Think
+                  of a news blog that's filled with content hourly on the day of
+                  going live. However, reviewers tend to be distracted by
+                  comprehensible content, say, a random text copied from a
+                  newspaper or the
+                </Segment>
 
-                <Sticky context={this.contextRef} offset={100}>
-                  <Header as="h3">Stuck Content</Header>
-                  <Image src="/images/wireframe/image.png" />
-                </Sticky>
-              </Rail>
-            </Segment>
+                <Rail attached position="left">
+                  <Sticky context={this.contextRef} offset={100}>
+                    In a professional context it often happens that private or
+                    corporate clients corder a publication to be made and
+                    presented with the actual content still not being ready.
+                    Think of a news blog that's filled with content hourly on
+                    the day of going live. However, reviewers tend to be
+                    distracted by comprehensible content, say, a random text
+                    copied from a newspaper or the internet. The are likely to
+                    focus on the text, disregarding the layout and its elements.
+                    Besides, random text risks to be unintendedly humorous or
+                    offensive, an unacceptable risk in corporate environments.
+                    Lorem ipsum and its many variants have been employed since
+                    the early 1960ies, and quite likely since the
+                  </Sticky>
+                </Rail>
+              </Grid.Column>
+            </Grid.Row>
           </Ref>
-        </Grid.Column>
-      </Grid>
+        </Grid>
+      </>
     );
   }
 }
