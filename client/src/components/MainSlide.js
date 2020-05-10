dd794452a311a748a078d2a1788368633ae8ae87
iTh1nk <fnchao@hotmail.com>
2020-05-09 18:04:13 -0700

    user-scalable=no

--- client/src/components/MainSlide.js
+++ client/src/components/MainSlide.js
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
-import {
-  Icon,
-  Menu,
-  Sidebar,
-  Grid,
-} from "semantic-ui-react";
+import { Icon, Menu, Sidebar, Grid } from "semantic-ui-react";
 import { AssignContext } from "./AssignContext";
 import HomeLeft from "./HomeLeft";
 import HomeRight from "./HomeRight";
@@ -72,27 +67,27 @@ export default function SlideBar() {
             setVisible(false);
           }}
         > */}
-              <AssignContext.Provider value={{ tabSwitch, setTabSwitch }}>
-            <Grid centered>
-                <Grid.Row>
-                  <Grid.Column id="home-top">
-                    <HomeTop />
-                  </Grid.Column>
-                </Grid.Row>
-                <hr id="hrDivider" />
-                <Grid.Row>
-                  <Grid.Column width={3} id="home-left">
-                    <HomeLeft />
-                  </Grid.Column>
-                  <Grid.Column id="home-mid"> </Grid.Column>
-                  <Grid.Column width={9} id="home-right">
-                    <HomeRight />
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row> </Grid.Row>
-            </Grid>
-              </AssignContext.Provider>
-        {/* </Sidebar.Pusher>
+      <AssignContext.Provider value={{ tabSwitch, setTabSwitch }}>
+        <Grid centered>
+          <Grid.Row>
+            <Grid.Column id="home-top">
+              <HomeTop />
+            </Grid.Column>
+          </Grid.Row>
+          <hr id="hrDivider" />
+          <Grid.Row>
+            <Grid.Column width={3} id="home-left">
+              <HomeLeft />
+            </Grid.Column>
+            <Grid.Column id="home-mid"> </Grid.Column>
+            <Grid.Column width={9} id="home-right">
+              <HomeRight />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row> </Grid.Row>
+        </Grid>
+      </AssignContext.Provider>
+      {/* </Sidebar.Pusher>
       </Sidebar.Pushable> */}
     </div>
   );
