2670fc6187d3360348c01f8e530f067bda12070c
iTh1nk <fnchao@hotmail.com>
2020-05-12 16:42:03 -0700

    complete CRUD

--- server/portfolio/api/urls.py
+++ server/portfolio/api/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import PostsListAPIView
+# from .views import PostsListAPIView
+from .views import get_post, get_posts, post_posts, delete_post, put_post
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    path('api/posts', PostsListAPIView.as_view())
+    # path('api/posts', PostsListAPIView.as_view()),
+    url(r'^api/posts/(?P<pk>\d+)$', views.get_post, name='get_post'),
+    url(r'^api/posts/$', views.get_posts, name='get_posts'),
+    url(r'^api/posts/add/$', views.post_posts, name='post_posts'),
+    url(r'^api/posts/del/(?P<pk>\d+)$', views.delete_post, name='delete_post'),
+    url(r'^api/posts/put/(?P<pk>\d+)$', views.put_post, name='put_post')
 ]
--- server/portfolio/api/views.py
+++ server/portfolio/api/views.py
@@ -2,14 +2,78 @@ from rest_framework.generics import ListAPIView
 from portfolio.models import Posts
 from .serializers import PostsSerializer
 
-
-class BaseListAPIView(ListAPIView):
-
-    def get_queryset(self):
-        return self.model.objects.all()
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class PostsListAPIView(BaseListAPIView):
+@api_view(['GET', 'DELETE', 'PUT'])
+def get_delete_update_post(request, pk):
+    try:
+        posts = Posts.objects.get(pk=pk)
+    except Posts.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    serializer_class = PostsSerializer
-    model = Posts
+    if request.method == 'GET':
+        return Response({})
+    elif request.method == 'DELETE':
+        return Response({})
+    elif request.method == 'PUT':
+        return Response({})
+
+
+@api_view(['GET'])
+def get_post(request, pk):
+    try:
+        post = Posts.objects.get(pk=pk)
+    except Posts.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = PostsSerializer(post)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_posts(request):
+    posts = Posts.objects.all()
+    serializer = PostsSerializer(posts, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def post_posts(request):
+    data = {
+        'title': request.data.get('title'),
+        'content': request.data.get('content'),
+        'author': request.data.get('author')
+    }
+    serializer = PostsSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def delete_post(request, pk):
+    try:
+        post = Posts.objects.get(pk=pk)
+    except Posts.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    post.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['PUT'])
+def put_post(request, pk):
+    try:
+        post = Posts.objects.get(pk=pk)
+    except Posts.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = PostsSerializer(post, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
