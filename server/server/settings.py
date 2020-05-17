73c494fa452834a01827bb0b2e3d01401fcb799d
iTh1nk <fnchao@hotmail.com>
2020-05-17 15:17:06 -0700

    collect static django

--- server/server/settings.py
+++ server/server/settings.py
@@ -167,7 +167,7 @@ CORS_ORIGIN_WHITELIST = [
 #         },
 #     },
 # ]
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'client', "build", "static"),
